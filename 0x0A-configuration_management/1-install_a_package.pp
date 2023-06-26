007b2b9e104296a19990c9ef00d6babcd5f1e7d7
Engy <engyalaa.ea.77@gmail.com>
2023-06-26 21:35:29 +0300

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
 # Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep 2.1.0',
+}
+
+#package { 'puppet-lint':
+#  ensure   => '2.5.0',
+#  provider => 'gem',
+#}
